8e7160ad136adde9dd4ac46bd197d45984671075
nagallegos <113269909+nagallegos@users.noreply.github.com>
2023-05-03 10:06:15 -0500

    Revert "Revert "Major updates to server.py and added .ico for page""
    This reverts commit 175bef1e703e7ba84cadfba17726d52992b97a92.

--- Wireshark_Activity/action.php
+++ Wireshark_Activity/action.php
@@ -2,7 +2,7 @@
 <body>
 
 Welcome <?php echo $_POST["username"]; ?><br>
-Your email address is: <?php echo $_POST["password"]; ?>
+Your password is: <?php echo $_POST["password"]; ?>
 
 </body>
 </html>
--- Wireshark_Activity/server.py
+++ Wireshark_Activity/server.py
@@ -9,45 +9,25 @@ Info:   https://docs.python.org/3/library/http.server.html
 """
 
 
-import http.server
+from http.server import CGIHTTPRequestHandler
 import socketserver
-import logging
-import cgi
 import ssl
-import os
 
 # Uncomment if you need to create a new key
 #os.system('cmd /k "openssl req -new -x509 -keyout server.pem -out ./key/server.pem -days 365 -nodes"')
 
-# Potentially need to update this entire class ***
-class ServerHandler(http.server.SimpleHTTPRequestHandler):
-
-    def do_GET(self):
-        logging.error(self.headers)
-        http.server.SimpleHTTPRequestHandler.do_GET(self)
-
-    # Need to update this post method ***
-    def do_POST(self):
-        logging.error(self.headers)
-        form = cgi.FieldStorage(
-            fp=self.rfile,
-            headers=self.headers,
-            environ={'REQUEST_METHOD':'POST',
-                     'CONTENT_TYPE':self.headers['Content-Type'],
-                     })
-        for item in form.list:
-            logging.error(item)
-        http.server.SimpleHTTPRequestHandler.do_GET(self)
-
-        with open("data.txt", "w") as file:
-            for key in form.keys(): 
-                file.write(str(form.getvalue(str(key))) + ",")
-
-IP = '192.168.1.106' # Change this to current IP Address
+IP = '172.20.10.9' # Change this to current IP Address
 PORT = 8000
-Handler = ServerHandler
+Handler = CGIHTTPRequestHandler
+document_root = './'
+
 with socketserver.TCPServer((IP, PORT), Handler) as httpd:
-    # The code below will "wrap" the socket to use TLS 1.3
+    # The code below will "wrap" the socket to use TLS
     #httpd.socket = ssl.wrap_socket (httpd.socket, certfile='./key/server.pem', server_side=True)
-    print("serving at port", PORT)
-    httpd.serve_forever()
+    try:
+        httpd.document_root = document_root
+        print(f'Starting server on {IP}:{PORT}')
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        httpd.server_close()
+        print('Stopping server...')
